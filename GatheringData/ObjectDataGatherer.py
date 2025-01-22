56a719d86f26748689c19038c98fa94802235958
Starskii <jakeylakie@yahoo.com>
2025-01-22 03:36:10 -0800

    Removed unnecessary print statement from ObjectDataGatherer

--- GatheringData/ObjectDataGatherer.py
+++ GatheringData/ObjectDataGatherer.py
@@ -5,7 +5,6 @@ from Utilities import utilities
 
 def gather_dataset(dataset_name: str, number_of_images: int, object_height_px: int, object_width_px: int,
                    image_capture_delay: float, update_function):
-    print("Starting")
     directory_path = f'./GatheringData/data/{dataset_name}'
     directory_path = utilities.create_directory(directory_path)
     for image_index in range(number_of_images):
