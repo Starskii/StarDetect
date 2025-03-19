7794e55b3875d7f827c863efb6abd443df86d9e9
Starskii <jakeylakie@yahoo.com>
2025-03-18 23:49:10 -0700

    added generate button to train tab

--- Controllers/TrainController.py
+++ Controllers/TrainController.py
@@ -9,3 +9,12 @@ class TrainController:
     def __init__(self, main_view: MainView, profile_manager: ProfileManager):
         self.train_tab = main_view.create_new_tab("Train")
         self.profile_manager = profile_manager
+        self.main_view = main_view
+        self.generate_training_set_button = self.main_view.add_button_to_tab(
+            self.train_tab,
+            "Generate",
+            self.generate_button_event
+        )
+
+    def generate_button_event(self):
+        print("GENERATE :D")
